ed56a817c17053e5c378d04f09c1b85c3e0cfd81
zhangkangning <zhangkangning@xiaohongshu.com>
2026-01-28 20:35:05 +0800

    tool_call增加长度惩戒, 1_28运行

--- examples/multi_task/train_composite_all.sh
+++ examples/multi_task/train_composite_all.sh
@@ -47,11 +47,11 @@ python3 -m examples.multi_task.train_composite \
     +data.code_num=0 \
     +data.bfcl_num=0 \
     +data.search_num=0 \
-    +data.tool_call_num=0 \
-    +data.local_search_num=12800 \
+    +data.tool_call_num=12800 \
+    +data.local_search_num=0 \
     +data.tool_call_data_path='/mnt/tidalfs-bdsz01/dataset/llm_dataset/zkn_data/rllm/rllm/data/datasets/tool_call_data' \
-    +data.dataset_name=m1_27_math0_code0_tool0_search0_localsearch1w \
-    trainer.experiment_name='m1_27_math0_code0_tool0_search0_localsearch1w-8b-mixed-tasks' \
+    +data.dataset_name=m1_28_math0_code0_tool1w_search0_localsearch0 \
+    trainer.experiment_name='m1_28_math0_code0_tool1w_search0_localsearch0-8b-mixed-tasks' \
     data.val_batch_size=512 \
     data.max_prompt_length=6400 \
     data.max_response_length=20480 \
